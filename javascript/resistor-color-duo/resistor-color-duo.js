669d172c59b4102d5020b3af52e08541c9794bb5
ThorrellT <thorrellt@gmail.com>
2022-02-15 21:46:09 -0500

    solution for exercism JS challenge - Resistor Color Duo

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const DECODE = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+}
+
+export const decodedValue = (colors) => {
+  return DECODE[colors[0]] * 10 + DECODE[colors[1]];
+};
